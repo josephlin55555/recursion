cb831b12b76391b103f1f31283449973e7c955bb
Joseph Lin <josephlin55555@gmail.com>
2015-03-06 15:04:17 -0800

    Main Requirements Completed

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,26 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+var getElementsByClassName = function(className) {
+	//console.log(document.body.childNodes[3].classList[0]);
+	var array = [];
+
+	function exploration(node) {
+
+		if(node.classList) { //checks if classList is correct
+			for(var i = 0; i < node.classList.length; i++) {
+				if(node.classList[i] === className) {
+					array.push(node);
+				}
+			}
+		}
+
+		for(var i = 0; i < node.childNodes.length; i++) {
+			exploration(node.childNodes[i]);
+		}
+
+	}
+
+	exploration(document.body);
+	return array;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,57 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+
+  if(typeof obj === 'object') {
+    if(Array.isArray(obj)) { //array
+      if(obj.length === 0) {
+        return "[]";
+      }
+      else {
+        var new_array = [];
+        for(var i = 0; i < obj.length; i++) {
+          new_array.push(stringifyJSON(obj[i]));
+        }
+
+        return '[' + new_array + ']'; //when converting to string, brackets are removed
+      }
+    }
+    else {
+      if(obj === null) { //null
+        return 'null';
+      }
+      else { //object
+        var result = [];
+        var temp = '';
+
+        for(var key in obj) {
+          if(stringifyJSON(obj[key])) {
+            result.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+          }
+        }
+
+        if(result.length === 0) {
+          temp = '{}';
+        }
+        else {
+          temp = '{' + result.join(',') + '}';
+        }
+
+        return temp;
+      }
+    }
+  }
+  else{ //number, string, boolean
+    if(typeof obj === 'string') {
+      var temp = '';
+      temp += '\"' + obj + '\"';
+      return temp;
+    }
+    else if(typeof obj === 'number') {
+      return '' + obj;
+    }
+    else if(typeof obj === 'boolean') {
+      return String(obj);
+    }
+  }
 };
